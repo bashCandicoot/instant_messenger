b5d094336fbe049ca26dbbfe32fec92f8247334f
steve <bashcandicoot@gmail.com>
2018-03-15 17:15:31 +0000

    init scripts

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+window.onload = function() {
+
+  var messagesEl = document.querySelector('.messages');
+  var typingSpeed = 40;
+  var loadingText = '<b>•</b><b>•</b><b>•</b>';
+  var messageIndex = 0;
+
+  var messages = [
+    'I had a dream about you last night',
+    'we were driving somewhere',
+    'I think your hand was on my thigh',
+    'or maybe mine was on yours..',
+    'mebbe',
+    'do you ever think about me?',
+    'I think about you occasionally tooo',
+    'like, sort of frequently really you see',
+    'i know you\'re not really, the same as what i imagine tho',
+    'because how could you be',
+    'people change right',
+    'I just mean, years and years have gone by',
+    'sooo.. the person i imagine isn\'t going to be how you are now',
+    'are you listening at all',
+    'how you always were',
+    'and I have made up conversations with you',
+    'like something will happen and then we will talk about it',
+    'or I will replay a memory but you\'ll be injected in it',
+    'a little bit',
+    'yeah',
+    'it\'s easy to get engrossed y\'know',
+    'like a dream',
+    'and it sucks when I realise it\'s not real',
+    'what do you mean?',
+  ]
+
+}
